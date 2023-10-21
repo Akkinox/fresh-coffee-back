7deddcb001b3356b686b7d23017f744542db8114
Ronal Olate Salamanca <ronal.olate1@gmail.com>
2023-10-21 20:32:06 -0300

    [003] Se agrega funcionalidad del controller auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\LogoutRequest;
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(RegistroRequest $request){
-        dd($request);
         //Validar el registro
         $data = $request->validated();
+
+        // Crear un usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        // Retornar respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
-    public function login(Request $request){
+
+    public function login(LoginRequest $request){
+
+        // Validacion de data
+        $data = $request->validated();
+
+        // Validacion de email y password
+        if(!Auth::attempt($data)){
+            return response([
+                'errors' => 'El email o la password son incorrectos'
+            ], 422);
+        }
+        // Autenticacion de usuario
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
 
     }
     public function logout(Request $request){
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
+        return [
+            'user' => null
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::apiResource('/categorias', CategoriaController::class);
@@ -26,3 +29,4 @@ Route::apiResource('/productos', ProductoController::class);
 
 //Autenticacion
 Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
